bdc786386f490b9f0700af75774586f1713c0a48
Wilson Shalo <wilsonshalo@Wilsons-MBP.attlocal.net>
2019-12-20 21:00:35 -0600

    fixed login bug

--- routes/auth.js
+++ routes/auth.js
@@ -39,7 +39,7 @@ module.exports = app => {
     // const account = await stripe.accountCards.list();
     // console.log(account);
     // res.send(account);
-    // const del = await stripe.accounts.del("acct_1FrEnNKPvE05k1yq");
+    // const del = await stripe.accounts.del("acct_1FrxCJDihoknFH71");
     // console.log(del);
     // stripe.accounts.retrieveExternalAccount(
     //   "acct_1FmQD2EWMyi6h2Gs",
