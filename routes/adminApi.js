dee005920fd8b9f0668046f1600962a8f0831f9e
Wilson Shalo <wilsonshalo@Wilsons-MBP.attlocal.net>
2019-12-20 21:06:40 -0600

    fixed login bug

--- routes/adminApi.js
+++ routes/adminApi.js
@@ -18,59 +18,58 @@ module.exports = app => {
     async (req, res) => {
       //find the user and update isActive to true
       const user = await User.findOne({ _id: req.params.partnerId });
-      console.log(user.address);
-      //const ssnSplit = user.ssnNumber.split("");
-      // const lastFour = ssnSplit
-      //   .splice(5, 8)
-      //   .toString()
-      //   .replace(/,/g, "");
+      const ssnSplit = user.ssnNumber.split("");
+      const lastFour = ssnSplit
+        .splice(5, 8)
+        .toString()
+        .replace(/,/g, "");
       //onbord the user for stripe connect
-      // const accountDetails = await stripe.accounts.create({
-      //   type: "custom",
-      //   country: "US",
-      //   business_type: "individual",
-      //   individual: {
-      //     first_name: user.fName,
-      //     last_name: user.lName,
-      //     phone: user.phone,
-      //     email: user.email,
-      //     ssn_last_4: lastFour,
-      //     id_number: user.ssnNumber,
-      //     address: {
-      //       city: user.locationCity,
-      //       country: "US",
-      //       line1: user.address,
-      //       line2: null,
-      //       postal_code: user.postal_code,
-      //       state: user.locationState
-      //     }
-      //   },
-      //
-      //   business_profile: {
-      //     mcc: "7230",
-      //     name: `${user.fName} ${user.lName}`,
-      //     product_description:
-      //       "I sell my beauty services on iBeautyConnect." + user.profession,
-      //     support_email: "ibeautyconnect2019@gmail.com",
-      //     support_phone: "+13124010122",
-      //     url: "https://www.facebook.com/ibeautyconnect/"
-      //   },
-      //   requested_capabilities: ["card_payments", "transfers"]
-      // });
+      const accountDetails = await stripe.accounts.create({
+        type: "custom",
+        country: "US",
+        business_type: "individual",
+        individual: {
+          first_name: user.fName,
+          last_name: user.lName,
+          phone: user.phone,
+          email: user.email,
+          ssn_last_4: lastFour,
+          id_number: user.ssnNumber,
+          address: {
+            city: user.locationCity,
+            country: "US",
+            line1: user.address,
+            line2: null,
+            postal_code: user.postal_code,
+            state: user.locationState
+          }
+        },
 
-      // user.isApproved = true;
-      // user.accountStatus = "approved";
-      // user.isApprovedNote = "account approved";
-      // user.stripeAccountId = accountDetails.id;
-      // user.staffHandler = req.params.staffName;
-      //
-      // //send the partner a welcome messsage
-      // messageBody = `Congratulation on your approval ${user.fName}. Welcome to the iBeautyConnect family. We are here for you. Open the iBeautyConnect app to get started iBeautyConnectPartner://get_started`;
-      // smsFunctions.sendSMS(req, res, user.phone, messageBody);
-      // await user.save();
-      // return httpRespond.authRespond(res, {
-      //   status: true
-      // });
+        business_profile: {
+          mcc: "7230",
+          name: `${user.fName} ${user.lName}`,
+          product_description:
+            "I sell my beauty services on iBeautyConnect." + user.profession,
+          support_email: "ibeautyconnect2019@gmail.com",
+          support_phone: "+13124010122",
+          url: "https://www.facebook.com/ibeautyconnect/"
+        },
+        requested_capabilities: ["card_payments", "transfers"]
+      });
+
+      user.isApproved = true;
+      user.accountStatus = "approved";
+      user.isApprovedNote = "account approved";
+      user.stripeAccountId = accountDetails.id;
+      user.staffHandler = req.params.staffName;
+
+      //send the partner a welcome messsage
+      messageBody = `Congratulation on your approval ${user.fName}. Welcome to the iBeautyConnect family. We are here for you. Open the iBeautyConnect app to get started iBeautyConnectPartner://get_started`;
+      smsFunctions.sendSMS(req, res, user.phone, messageBody);
+      await user.save();
+      return httpRespond.authRespond(res, {
+        status: true
+      });
     }
   );
   app.post(
