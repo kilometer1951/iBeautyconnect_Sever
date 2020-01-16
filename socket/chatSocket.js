01ff072785a889e5cf5a95b8107978d4515ad510
Wilson Shalo <wilsonshalo@Wilsons-MacBook-Pro.local>
2020-01-16 16:14:19 -0600

    adupload api and other api's

--- socket/chatSocket.js
+++ socket/chatSocket.js
@@ -10,7 +10,59 @@ module.exports = (app, io) => {
 
     socket.on("newMessage", async function(msg) {
       io.emit("newMessage", { msg });
-      console.log(msg);
+      //save message
+      const message = await Message.findOne({
+        client: msg.clientId,
+        partner: msg.partnerId,
+        deleted: false
+      });
+
+      if (msg.type == "reschedule") {
+        //send sms
+        messageBody =
+          msg.message +
+          ". Open the iBeautyConnect app to respond to your clients request. iBeautyConnectPartner://messages thanks.";
+        smsFunctions.sendSMS("req", "res", msg.partnerPhone, messageBody);
+      }
+
+      if (message) {
+        //update message
+        message.recentMesage = msg.message;
+        if (msg.to == msg.partnerId) {
+          message.partnerHasViewed = false;
+          message.clientHasViewMessage = true;
+        } else {
+          message.clientHasViewMessage = false;
+          message.partnerHasViewed = true;
+        }
+        message.dateModified = new Date();
+        message.save();
+        const update_message = {
+          message: msg.message,
+          to: msg.to,
+          from: msg.from
+        };
+        await Message.update(
+          { _id: message._id },
+          {
+            $push: {
+              message_data: update_message
+            }
+          }
+        );
+      } else {
+        newMessage = {
+          client: msg.clientId,
+          partner: msg.partnerId,
+          recentMesage: msg.message,
+          message_data: {
+            message: msg.message,
+            to: msg.to,
+            from: msg.from
+          }
+        };
+        new Message(newMessage).save();
+      }
     });
 
     // socket.on("m", function(msg) {
