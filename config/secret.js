934b0c635fc3dc92833b456f51e1d1d640dab39c
Wilson Shalo <wilsonshalo@Wilsons-MBP.attlocal.net>
2020-03-04 13:07:20 -0600

    adcompleted client api server

--- config/secret.js
+++ config/secret.js
@@ -1,8 +1,10 @@
 module.exports = {
   database:
-    "mongodb+srv://root:Louis1951@cluster0-15w62.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb://root:Louis1951@ds033639.mlab.com:33639/ibeautyconnectpartner",
   TwiloAccountSid: "AC2eee9d2fafa59aa8751561dee2804fd0",
   TwiloAuthToken: "f27b1b27e699209516aead379e79361d",
   PushNotificationKEYID: "HP5C6549F2",
   AppleTeamID: "TRBNA383H2"
 };
+
+// mongodb+srv://root:Louis1951@cluster0-15w62.mongodb.net/test?retryWrites=true&w=majority
