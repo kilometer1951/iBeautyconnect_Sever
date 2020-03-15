40c95dab1a1d4b5f731cdc45bf72ee18c0fef5fd
Wilson Shalo <wilsonshalo@wilsons-mbp.attlocal.net>
2020-03-15 07:52:49 -0500

    adcompleted client api server

--- routes/partner/authPartner.js
+++ routes/partner/authPartner.js
@@ -39,7 +39,7 @@ module.exports = app => {
     // const account = await stripe.accountCards.list();
     // console.log(account);
     // res.send(account);
-    // await stripe.accounts.del("acct_1GJQqxEsToFKmMPE");
+    // await stripe.accounts.del("acct_1GMaQVEy5mZHXP0w");
     // res.send(true);
     // stripe.accounts.retrieveExternalAccount(
     //   "acct_1FmQD2EWMyi6h2Gs",
